e9e54858edd2363292ddde91d7054abffbcaad05
ALexander4295502 <yuanzhengstl@gmail.com>
2017-07-05 12:38:04 -0500

    Add spear weapon [Not Finished]

--- 2D_Adventure/Assets/ArmRotation.cs
+++ 2D_Adventure/Assets/ArmRotation.cs
@@ -6,12 +6,18 @@ public class ArmRotation : MonoBehaviour {
 	
 	public float rotationOffset = 0f;
 
+	[SerializeField] private GameObject weaponInHand;
+
 	// Update is called once per frame
 	void Update () {
-		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-		difference.Normalize();
 
-		float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(0f,0f,rotZ+rotationOffset);
+		if(weaponInHand.CompareTag("LongDistanceWeapon")){
+			Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			difference.Normalize();
+
+			float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler(0f,0f,rotZ+rotationOffset);
+		}
+
 	}
 }
