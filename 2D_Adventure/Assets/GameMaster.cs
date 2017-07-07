61ef42ec21335733d843824d5c02b22827b9f594
ALexander4295502 <yuanzhengstl@gmail.com>
2017-07-07 12:48:17 -0500

    Add audioManger and modify GameMaster

---
+++ 2D_Adventure/Assets/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gm;
+
+	private AudioManager audioManager;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		if (gm == null)
+		{
+			gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		audioManager = AudioManager.instance;
+		if(audioManager == null){
+			Debug.LogError("No AudioManager found in this scene.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 2D_Adventure/Assets/WeaponShort.cs
+++ 2D_Adventure/Assets/WeaponShort.cs
@@ -6,6 +6,9 @@ public class WeaponShort : MonoBehaviour {
 
 	private float fireRate = 2;
 
+	[SerializeField] private float comboDelay = 1f;
+	private float comboTimer = 0;
+
 	[SerializeField] private Animator playerAnim;
 	[SerializeField] private XftWeapon.XWeaponTrail trailEffect;
 	[SerializeField] private GameObject hitParticleEffect;
@@ -23,15 +26,23 @@ public class WeaponShort : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton ("Fire1") && Time.time > timeToFire && playerAnim.transform.GetComponent<Rigidbody2D>().velocity.y == 0) {
+		if( comboTimer > 0 ){
+             comboTimer += Time.deltaTime ;
+		}
+		// Do not allow combo if the user waited to long
+        if( comboTimer > comboDelay ){
+             comboTimer = 0 ;
+		}
+		if (Input.GetButton ("Fire1") && Time.time > timeToFire ) {
 			timeToFire = Time.time + 1/fireRate;
 			damageSettle = true;
-			Attack();
+			AttackAnim();
 		}
+
 		if(playerAnim.GetCurrentAnimatorStateInfo(0).IsTag("Attack")){
 			trailEffect.MyColor.a = 255;
 			if(damageSettle == false){
-				LaunchAttack(GetComponent<PolygonCollider2D>());
+				LaunchAttack(GetComponent<Collider2D>());
 				damageSettle = true;
 			}
 		}else{
@@ -41,34 +52,35 @@ public class WeaponShort : MonoBehaviour {
 		}
 	}
 
-	private void Attack(){
-		playerAnim.SetTrigger("SDWAttack");
+	private void AttackAnim(){
+		// Simple attack
+		if( comboTimer == 0 )
+		{
+			playerAnim.SetTrigger("SDWAttack1");
+			comboTimer += Time.deltaTime ;
+		} else if( comboTimer <= comboDelay )
+		{
+			playerAnim.SetTrigger("SDWAttack2");
+			// reset timer to come back to simple attack
+			comboTimer = 0 ;
+		}
 	}
 
-	// /// <summary>
-	// /// Sent when another object enters a trigger collider attached to this
-	// /// object (2D physics only).
-	// /// </summary>
-	// /// <param name="other">The other Collider2D involved in this collision.</param>
-	// void OnTriggerEnter2D(Collider2D other)
-	// {
-	// 	if(other.CompareTag("Item")){
-	// 		hitParticleEffect.SetActive(true);
-	// 	}
-	// }
-
 	private void LaunchAttack(Collider2D col){
-
-		Debug.Log("Launch attack!!!!");
 
 		Vector2 _center = new Vector2(col.bounds.center.x, col.bounds.center.y);
 		Vector2 _size = new Vector2(col.bounds.extents.x, col.bounds.extents.y);
 		Collider2D _col = Physics2D.OverlapBox(_center, _size, col.transform.rotation.z, LayerMask.GetMask("Item"));
-
+		
 		if(_col != null && _col.CompareTag("Item")){
-			Instantiate(hitParticleEffect, col.bounds.center, (playerAnim.transform.position.x > _col.transform.position.x)? Quaternion.Euler(0f,-90f,0f) : Quaternion.Euler(0f,90f,0f));
-			Instantiate(hitBlood, col.bounds.center, (playerAnim.transform.position.x > _col.transform.position.x)? Quaternion.Euler(0f,-90f,0f) : Quaternion.Euler(0f,90f,0f));		
+			HitEnemy(col, _col);
 		}
 		
+	}
+
+	private void HitEnemy(Collider2D col, Collider2D _col){
+		Debug.Log("Hit "+_col.name);
+		Instantiate(hitParticleEffect, col.bounds.center, (playerAnim.transform.position.x > _col.transform.position.x)? Quaternion.Euler(0f,-90f,0f) : Quaternion.Euler(0f,90f,0f));
+		Instantiate(hitBlood, col.bounds.center, (playerAnim.transform.position.x > _col.transform.position.x)? Quaternion.Euler(0f,-90f,0f) : Quaternion.Euler(0f,90f,0f));		
 	}
 }
